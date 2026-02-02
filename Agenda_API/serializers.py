5f095faae95634a5969372fe7a61037fce86f215
Retr042 <rodrigoreyes42@aragon.unam.mx>
2026-02-01 19:14:42 -0600

    2do Commit: Adicion de Create, Update y delete en serializers

--- Agenda_API/serializers.py
+++ Agenda_API/serializers.py
@@ -13,8 +13,8 @@ class DireccionSerializer(serializers.ModelSerializer):
 
 class ContactoSerializer(serializers.ModelSerializer):
     #relaciones 
-    direccion = DireccionSerializer(read_only=True)
-    telefonos = TelefonoSerializer(read_only=True, many=True)
+    direccion = DireccionSerializer()
+    telefonos = TelefonoSerializer(many=True)
     class Meta:
         model = Contacto
         fields = ['nombre', 'apellidos', 'fotografia', 'fecha_nac', 'direccion', 'telefonos']
@@ -23,4 +23,38 @@ class ContactoSerializer(serializers.ModelSerializer):
         data = super().to_representation(instance)
         return {
             'Contacto' : data
-        }    
+        }
+
+    def create(self, validated_data):
+
+        direccion_data =  validated_data.pop('direccion') 
+        telefonos_data = validated_data.pop('telefonos')  
+
+        contacto = Contacto.objects.create(**validated_data)
+
+        Direccion.objects.create(contacto=contacto,**direccion_data)
+
+        for tel_data in telefonos_data:
+            Telefono.objects.create(contacto=contacto, **tel_data)
+        return contacto
+    
+    def update(self, instance, validated_data):
+        direccion_data = validated_data.pop('direccion', None)
+        telefonos_data = validated_data.pop('telefonos', None)
+
+        instance.nombre = validated_data.get('nombre', instance.nombre)
+        instance.apellidos = validated_data.get('apellidos', instance.apellidos)
+        instance.fotografia = validated_data.get('fotografia', instance.fotografia)
+        instance.fecha_nac = validated_data.get('fecha_nac', instance.fecha_nac)
+        instance.save()
+
+        if direccion_data:
+            direccion = instance.direccion
+            for attr, value in direccion_data.items():
+                setattr(direccion, attr, value)
+            direccion.save()
+        if telefonos_data is not None:
+            instance.telefonos.all().delete()
+            for tel in telefonos_data:
+                Telefono.objects.create(contacto=instance, **tel)
+            return instance            
